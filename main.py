c925c6b48f1419103b42ad343b861d4842ede477
Pablopalombo <89134094+Pablopalombo@users.noreply.github.com>
2023-11-14 23:40:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pandas as pd
 from configparser import ConfigParser
 import sqlalchemy as sa
 
-config = ConfigParser()
-config.read("config/config.ini")
+config = ConfigParser()  ##se configura para parsear el archivo config
+config.read("config/config.ini")  #se instancia la lectura 
 
-config_db = config['DataBase']
+config_db = config['DataBase']  ##traen los datos referidos a la base y debajo lo mismo respecto a la contraseña de la api
 host = config_db['host']
 port = config_db['port']
 dbname = config_db['dbname']
@@ -17,11 +17,11 @@ config_api = config['api']
 secret_key = config_api["secret_key"]
 
 def conn_string():
-    return f'postgresql://{username}:{password}@{host}:{port}/{dbname}'
+    return f'postgresql://{username}:{password}@{host}:{port}/{dbname}' ##se anexan las variables para conformar la URL
 
-def create(url):
-    engine = sa.create_engine(url) 
-    conn = engine.connect() ## se conecta pero no hace nada. 
+def create(url): #función que se conecta a la base de datos y crea la tabla. Toma como parámetro la URL
+    engine = sa.create_engine(url)  se usa la librería de SQLALCHEMY para sentenciar la URL como motor para conectar
+    conn = engine.connect() ##se usa el motor para conectar
     conn.execute(f"""
             DROP TABLE IF EXISTS palombopabloe_coderhouse.api;
             CREATE TABLE palombopabloe_coderhouse.api (
@@ -43,39 +43,39 @@ def create(url):
                 date TIMESTAMP
             );
         """
-    )
-    conn.close()
+    ) ##ya conectado, ejecutas la query que, en caso de no estar creada, crea la tabla.
+    conn.close() ##luego, se cierra la conexión.
 
-def rename_columns(df):
+def rename_columns(df): #incorporación respecto al tp anterior, se cambian las columnas para incorporar una etapa de ETL. 
     columns = ["open", "high", "low", "close"]
     for name in columns:
         df.rename(columns={name: name+"_price"}, inplace=True)
 
-def add(url, data):
-    engine = sa.create_engine(url)
-    conn = engine.connect()
+def add(url, data): #función que incluye dos parámetros, URL y data. Ambas se declaran después. 
+    engine = sa.create_engine(url) #se crea la variable que corre el motor de la base con el URL declarado. 
+    conn = engine.connect() #se crea otra variable que representa al conexión de la variable anterior. 
     
-    data.to_sql(name="api",
+    data.to_sql(name="api", #incorpora los datos bajo la función to_sql, que requiere se le indique el nombre de la tabla, la variable que representa la conexión, el esquema. 
     con=conn,
     schema="palombopabloe_coderhouse",
-    if_exists="append",
+    if_exists="append", #si existe una tabla, que haga un append
     method="multi",
-    index=False, ##ya tenemos el id que se genera
+    index=False, ##indica que no genere un índice. 
     )
 
 
 
-if __name__ == "__main__":
-    url = conn_string()
+if __name__ == "__main__": # el main representa lo primero que se ejecuta
+    url = conn_string() #se indica que la variable URL toma la URL que surge de la función conn_String
 
     params = {
-    'access_key': secret_key
-    } ##necesita que se llame access key
+    'access_key': secret_key #se invoca el parámetro del archivo confing.
+    } 
 
-    api_result = requests.get('http://api.marketstack.com/v1/tickers/aapl/eod/latest', params)
-    api_response = api_result.json()
-    df = pd.DataFrame([api_response]) ##van corchetes, para lista
+    api_result = requests.get('http://api.marketstack.com/v1/tickers/aapl/eod/latest', params)  #indica que la variable denominada resultado surge de ejecutar la API
+    api_response = api_result.json()  #luego lo ejecutado se pasa a json
+    df = pd.DataFrame([api_response]) ##van corchetes, para lista #luego se pasa a un dataframe
 
-    rename_columns(df)
-    add(url, df)
+    rename_columns(df) #se invoca la función que cambia las columnas
+    add(url, df) #se invoca la función que toma los dos parámetros, el primero es la conexión a la base y crea la tabla, el segundo incorpora el dataframe
 
